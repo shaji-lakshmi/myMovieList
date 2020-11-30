eda9f25225f802081407d4dca27748069869b853
JustAJovie <60591411+JustAJovie@users.noreply.github.com>
2020-11-29 21:36:18 -0500

    index.vue works

--- ljMovieList/pages/index.vue
+++ ljMovieList/pages/index.vue
@@ -1,25 +1,40 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">bezKoder</router-link>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/tutorialList" class="nav-link">Tutorials</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/Addtutorial" class="nav-link">Add</router-link>
-        </li>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        My Movie Mania
+      </h1>
+      <div class="links">
+        <a
+          href="homePage"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+         Enter Site
+        </a>
       </div>
-    </nav>
-
-    <div class="container mt-3">
-      <router-view />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  async fetch () {
+    const post = await fetch('http://localhost:8080/api/tutorials')
+      .then(res => res.json())
+    console.log(post)
+
+    // if (post.id === this.$route.params.id) {
+    //   this.post = post
+    // } else {
+    //   // set status code on server and
+    //   if (process.server) {
+    //     this.$nuxt.context.res.statusCode = 404
+    //   }
+    //   // use throw new Error()
+    //   throw new Error('Post not found')
+    // }
+  }
 }
 </script>
